95efcbcfba14d3049c27bf80f76f099f0e82056d
vitalish <vitali.shcherban@gmail.com>
2023-08-28 19:09:17 +0300

    Initial commit

---
+++ algorithms/src/main/java/com/vitalish/kata/alogorithms/sort/BubbleSort.java
@@ -0,0 +1,18 @@
+package com.vitalish.kata.alogorithms.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 6, 5, 1, 9, 7, 10};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void sort(int[] arr) {
+        // todo
+    }
+
+
+}
