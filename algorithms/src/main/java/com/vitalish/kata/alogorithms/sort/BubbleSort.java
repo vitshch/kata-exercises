7a8066ab93ce4ec5d0f746dc05a9f88032b7f38a
vitalish <vitali.shcherban@gmail.com>
2023-08-28 19:10:36 +0300

    Bubble Sort algorithm

--- algorithms/src/main/java/com/vitalish/kata/alogorithms/sort/BubbleSort.java
+++ algorithms/src/main/java/com/vitalish/kata/alogorithms/sort/BubbleSort.java
@@ -11,7 +11,15 @@ public class BubbleSort {
     }
 
     private static void sort(int[] arr) {
-        // todo
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    var temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 
 
